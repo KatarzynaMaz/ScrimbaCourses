3232192f8ddb8fe33cbea0fc45428f0e4f25ce83
Katarzyna Mazur <mazur66@hotmail.com>
2020-07-19 23:01:37 -0400

    Practicing Functions

---
+++ JSForBeginners/PracticeFunctions/app.js
@@ -0,0 +1,13 @@
+function doMath(num){
+    let newNum = (num + 100)/10;
+    return newNum;
+}
+
+let numOne = doMath(20);
+console.log(numOne);
+
+let numTwo = doMath(40);
+console.log(numTwo);
+
+let numThree = doMath(60);
+console.log(numThree);

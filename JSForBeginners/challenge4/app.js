8ab93803168c2b4323a0900c468b28dba7cc2a56
Katarzyna Mazur <mazur66@hotmail.com>
2020-07-24 00:04:11 -0400

    Fixed the Calculate button

--- JSForBeginners/challenge4/app.js
+++ JSForBeginners/challenge4/app.js
@@ -3,13 +3,29 @@
 //Make tip parameter optional
 //Return a total amount and save it to a variable
 
-function caclulateTip(chargeAmount, tip = 0.10){
+/*function caclulateTip(chargeAmount, tip = 0.10){
     let tipAmount = chargeAmount * tip;
     totalAmount = chargeAmount + tipAmount;
     return totalAmount;
 }
 
-let firstBill =caclulateTip(150);
+let totalBill =caclulateTip(75);
     
-    console.log(" You need to pay " + "$" + firstBill);
+    console.log(" You need to pay " + "$" + totalBill);*/
 
+function calculateTotalBill (){
+    let tip = document.getElementById("tip").value;
+    //tip = tip*0.1;
+   
+
+    let billAmount = document.getElementById("billAmount").value;
+    billAmount = Number(billAmount);
+
+    let tipAmount = billAmount*(tip/100);
+    tipAmount = Number(tipAmount);
+
+    let amountToPay = document.getElementById("amountToPay");
+  
+    totalBill = billAmount + tipAmount;
+    amountToPay.innerText = totalBill;
+}

27ab2f650bb6a44605258ea4c71a17729446122c
Katarzyna Mazur <mazur66@hotmail.com>
2020-07-26 10:14:23 -0400

    Coded a simple calculator when you add a number to user's input

--- JSForBeginners/PacticeJQuery/app.js
+++ JSForBeginners/PacticeJQuery/app.js
@@ -1,8 +1,20 @@
 /*let text = document.querySelector(".someText");
 text.value ="Tatry";*/
 
+ /* creting a basic click event
     let clickTotal=0;
     function ClickMe(){
         console.log("You clicked me " + clickTotal + " times");
         clickTotal++;
+    }*/
+    let number = 100;
+    function addHundred(){
+               let inputNumber = document.querySelector(".inputNumber");
+               let value = Number(inputNumber.value);
+               let totalValue = value + number;
+
+               let h1 = document.querySelector("h1");
+               let span = h1.querySelector("span");
+
+               span.innerHTML= totalValue;
     }

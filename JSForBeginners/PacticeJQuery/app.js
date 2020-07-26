cccf5166cd57813047b93aeedbcb4c2d313ce690
Katarzyna Mazur <mazur66@hotmail.com>
2020-07-26 08:19:43 -0400

    Practice creating an event

--- JSForBeginners/PacticeJQuery/app.js
+++ JSForBeginners/PacticeJQuery/app.js
@@ -1,2 +1,8 @@
-let text = document.querySelector(".someText");
-text.value ="Adirondacks";
+/*let text = document.querySelector(".someText");
+text.value ="Tatry";*/
+
+    let clickTotal=0;
+    function ClickMe(){
+        console.log("You clicked me " + clickTotal + " times");
+        clickTotal++;
+    }
